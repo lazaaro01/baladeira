daaf2fb1c7c19a60be6fc0e2b57f5f3a39cb7540
Lázaro Vasconcelos <152433905+lazaaro01@users.noreply.github.com>
2025-06-16 20:06:32 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = "cacanicks-cache-v1";
+const CACHE_NAME = "memoria-cache-v1";
 const urlsToCache = [
     "/",
-    "/index.html",
+    "/memoria.html",
     "/manifest.json",
     "/icon-192.png",
     "/icon-512.png",
@@ -10,7 +10,17 @@ const urlsToCache = [
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
+            console.log('Cacheando arquivos')
             return cache.addAll(urlsToCache);
         })
     );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
 });
